b822fe68c971883c5ecd3f530bb00dd803dbb561
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-03 10:11:03 +0530

    the simple date time picker ui

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonDatetime,
+  IonDatetimeButton,
+  IonHeader,
+  IonModal,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { useState } from "react";
 
 const Tab1: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +30,46 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          {/* <IonButton onClick={() => setShowModal(true)} expand="block">
+            Enter Date and TIme
+          </IonButton> */}
+          <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
+
+          <IonModal keepContentsMounted={true}>
+            <IonDatetime id="datetime"></IonDatetime>
+          </IonModal>
+        </div>
+
+        {/* {showModal && <IonDatetime />} */}
+        <IonModal isOpen={showModal}>
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="start">
+                <IonButton color="medium" onClick={() => setShowModal(false)}>
+                  Cancel
+                </IonButton>
+              </IonButtons>
+              {/* <IonTitle>Modal</IonTitle> */}
+              <IonButtons slot="end">
+                <IonButton onClick={() => setShowModal(false)}>
+                  Confirm
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+
+          <IonDatetime />
+        </IonModal>
+
+        {/* <ExploreContainer name="Tab 1 page" /> */}
       </IonContent>
     </IonPage>
   );
